8c40a50260860d1ac941071df0ba1c594589f9bf
yc757 <yctsai8698@gmail.com>
2022-04-12 21:40:16 +0800

    First version of application moved into github

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-pl@uejqv0e&#y-+bmb#5o9nk&)ruk59h9#c2=6)%*=a2c!e()$'
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
@@ -28,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
-ALLOWED_HOSTS = ['heroku-django-yc.herokuapp.com']
+ALLOWED_HOSTS = ['heroku-django-yc.herokuapp.com','127.0.0.1']
 
 
 # Application definition
